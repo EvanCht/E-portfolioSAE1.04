8f1577fa3bd93bfbdc703ce3a0bf7a8ea395e34c
EvanCht <evan.chavot@etu.univ-st-etienne.fr>
2026-05-11 14:58:29 +0200

    correction du CV + ajout du 5ème point de la page mise en pratique + création du controller analyse

---
+++ src/Controller/AnalysecontrollerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AnalysecontrollerController extends AbstractController
+{
+    #[Route('/analysecontroller', name: 'app_analysecontroller')]
+    public function index(): Response
+    {
+        return $this->render('analysecontroller/index.html.twig', [
+            'controller_name' => 'AnalysecontrollerController',
+        ]);
+    }
+}
